bd64b9a25b345683495eee25de728df2d9b6ac1b
MRiabov <maksymriabov2004@gmail.com>
2022-10-25 16:13:59 +0200

    Created a structure of the project

---
+++ src/main/java/edu/mriabov/restapitesttask/RestApiTestTaskApplication.java
@@ -0,0 +1,13 @@
+package edu.mriabov.restapitesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mriabov/restapitesttask/RestApiTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mriabov.restapitesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
